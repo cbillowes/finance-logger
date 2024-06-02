8592cb3a76a077c9ece783c1b996c546f7f8490f
Clarice Bouwer <cbillowes@gmail.com>
2024-06-02 06:07:57 +0400

    Log formatted document one add of invoice/payment selection

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import { Invoice } from "./classes/invoice.js";
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { Formatter } from "./interfaces/Formatter.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type") as HTMLSelectElement;
-const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const toOrFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
+  let document: Formatter;
 
-  const invoice = new Invoice(
-    tofrom.value,
-    details.value,
-    amount.valueAsNumber
-  );
+  if (type.value === "invoice") {
+    document = new Invoice(toOrFrom.value, details.value, amount.valueAsNumber);
+  } else {
+    document = new Payment(toOrFrom.value, details.value, amount.valueAsNumber);
+  }
 
-  console.log(invoice);
+  console.log(document.format());
 });
